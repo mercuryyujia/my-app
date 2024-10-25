7113039c00e2504f10e597434cf58615c0395652
mercuryyujia <yf2148@nyu.edu>
2024-10-26 00:09:31 +0800

    ver5

--- src/components/points.js
+++ src/components/points.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 function Points(props) {
-    const {data, xScale, yScale, height, width} = props;
+    const { data, xScale, yScale, selectedStation, setSelectedStation, setTooltipData, setTooltipX, setTooltipY } = props;
     //Note: 
     //the if(data){...} means when data is not null, the component will return the points; otherwise, it returns <g></g>
     //we use the if ... else ... in this place so that the code can work with the SSR in Next.js;
--- src/components/scatterPlot.js
+++ src/components/scatterPlot.js
@@ -11,9 +11,9 @@ function ScatterPlot(props){
     //task2: import the components needed and uncomment the components in the return 
     return (
         <g transform={`translate(${offsetX}, ${offsetY})`}>
-            <Points data={data} xScale={xScale} yScale={yScale} height={height} width={width} />
-            <YAxis yScale={yScale} height={height} axisLabel={"Trip duration end in"} />
-            <XAxis xScale={xScale} height={height} width={width} axisLabel={"Trip duration start from"} />
+            <Points data={data} xScale={xScale} yScale={yScale} radius={5} color='steelblue' selectedStation = {selectedStation} setSelectedStation = {setSelectedStation} setTooltipData={setTooltipData} setTooltipX={setTooltipX} setTooltipY={setTooltipY}/>
+            <YAxis xScale = {xScale} yScale={yScale} axisLabel="Trip duration ended in" />
+            <XAxis xScale={xScale} height={height} width={width} axisLabel={"Trip duration started from"} />
         </g>
     );
 }
