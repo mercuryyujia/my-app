464eb8767067fff1cb6241f55d29448bd910254d
mercuryyujia <yf2148@nyu.edu>
2024-10-25 22:55:25 +0800

    ver3

--- src/pages/assignment4_YujiaFu.js
+++ src/pages/assignment4_YujiaFu.js
@@ -1,7 +1,6 @@
 
 import React, { useState } from 'react';
 import * as d3 from 'd3';
-import 'bootstrap/dist/css/bootstrap.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import ScatterPlot from '../components/scatterPlot';
 import BarChart from '../components/barChart';
