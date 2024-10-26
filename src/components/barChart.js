84fe8df7900f06673011ed55181dda762af4fcc3
mercuryyujia <yf2148@nyu.edu>
2024-10-26 10:26:26 +0800

    label_fixed

--- src/components/barChart.js
+++ src/components/barChart.js
@@ -9,7 +9,7 @@ function BarChart(props){
     //task2: import the components needed and uncomment the components in the return 
     return <g transform = {`translate(${offsetX}, ${offsetY})`}>
         <Bars data={data} xScale={xScale} yScale={yScale} height={height} selectedStation = {selectedStation} setSelectedStation = {setSelectedStation}/>
-        <YAxis xScale={xScale} yScale={yScale} width={width} axisLable={"Bikers start from"}/>
+        <YAxis xScale={xScale} yScale={yScale} width={width} axisLabel={"Bikers start from"}/>
         <XAxis xScale={xScale} height={height} width={width} />
         </g>
 }
