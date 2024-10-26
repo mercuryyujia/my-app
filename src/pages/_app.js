20ead3d3369f934db33c677b7b01fa4d893db8c7
mercuryyujia <yf2148@nyu.edu>
2024-10-26 12:11:30 +0800

    ver6

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
-
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+// Import the ErrorBoundary component
+import ErrorBoundary from '../components/ErrorBoundary'
+ 
+function MyApp({ Component, pageProps }) {
+  return (
+    // Wrap the Component prop with ErrorBoundary component
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  )
 }
+ 
+export default MyApp
+
+//export default function App({ Component, pageProps }) {
+//  return <Component {...pageProps} />;
+//}
